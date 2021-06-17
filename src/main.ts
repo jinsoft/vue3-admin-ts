381473499a2ad211ef722e21828dd74e4459e3a8
xiaojin <admin@ainiok.com>
2021-06-17 18:31:10 +0800

    add login page

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
+import '../theme/F89E21/index.css';
+import ElementPlus from 'element-plus';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -6,6 +8,7 @@ import store from "./store";
 const app = createApp(App);
 
 app
+  .use(ElementPlus)
   .use(store)
   .use(router)
   .mount("#app");
